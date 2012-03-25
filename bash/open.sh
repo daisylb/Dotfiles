3ea061fb2a91d5d49479a1cf27fdf9a30cbdf1ea
Adam Brenecki <adam@brenecki.id.au>
2012-03-25 18:21:11 +1030

    Added tools to open files

---
+++ bash/open.sh
@@ -0,0 +1,24 @@
+o(){
+	if [ "$1" == "" ]; then
+		xdg-open . &> /dev/null
+	else
+		xdg-open "$1" &> /dev/null
+	fi
+}
+
+e(){
+	nohup sublime_text $1 >/dev/null 2>&1 &
+}
+
+g(){
+	nohup $* >/dev/null 2>&1 &
+}
+
+sp(){
+	g sublime_text --project ~/.sublime_projects/$1.sublime-project
+}
+
+GITG_PATH=`which gitg`
+alias gitg='g $GITG_PATH'
+
+alias clean-gedit-tempfiles='find . -name "*~" | xargs rm'
