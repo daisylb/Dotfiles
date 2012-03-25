b06fcf29554cde5bb19e55812c2db277a4f226b8
Adam Brenecki <adam@brenecki.id.au>
2012-03-25 19:00:40 +1030

    Update csem_local to use ssh config, and comment out print function because UniCard breaks it.

---
+++ bash/csem_local.markdown
@@ -0,0 +1,36 @@
+# Macros for Flinders University School of Computer Science, Engineering and Mathematics
+
+## Installation:
+
+1. Download `https://raw.github.com/adambrenecki/Dotfiles/master/bash/csem_local.sh` and put it somewhere.
+2. Add the following to your `.ssh/config` (create it if it doesn't exist):
+
+        Host csem
+          Hostname lofty.csem.flinders.edu.au
+          User bren0094
+
+2. Add the following to your `.bashrc`:
+
+        source /absolute/path/to/csem_local.sh
+
+3. Restart Bash for the settings to take effect.
+4. Set up passwordless login:
+   * If you don't already have a RSA key, run `ssh-keygen -t rsa`. 
+   * Run `ssh-copy-id csem`.
+
+## Usage
+
+### Provided commands
+
+* `csem` - Runs an interactive Bash shell on the CSEM server.
+* `csem-x` - Connects to the CSEM SSH server, using X, so you can run graphical commands remotely. Expect this to be slow as molasses unless you are on campus.
+* `csem-try <task>` (for CP2 with Paul Calder, possibly other topics too) - Bundles up all the `*.cpp` and `*.h` files in the current directory, and runs them through the `try` tool for the given task.
+* `csem-print <file> [<printer>]` Prints `file` (a PDF) to the given printer. If printer is not specified, defaults to `min`. **WARNING: Printing PDFs using `lp` no longer appears to work with the introduction of UniCard, for me at least. This function has been commented out for now, uncomment if you want to risk getting charged for 80 pages of garbage.**
+
+### Other things you can do
+
+These functions to be macros for convenience before I switched to using `.ssh/config`.
+
+* `ssh csem` - Log in to CSEM and get their standard shell (zsh?)
+* `scp [-r] <myfile> csem:[<directory>]` - Copy `myfile` to `directory` on the CSEM systems, or your home directory if not specified (make sure the `:` stays there). Use `-r` to copy entire directories.
+* `scp [-r] csem:<myfile> <path-to-copy-to>` - Copy `myfile` on the CSEM systems to `path-to-copy-to` (to copy to your current working directory, use `.`).q
--- bash/csem_local.sh
+++ bash/csem_local.sh
@@ -1,98 +1,24 @@
-# Installation:
-#
-# 1. Download this file.
-# 2. Add the following to your .bash_profile:
-#    FAN=blog1234 #replace with your FAN
-#    source /absolute/path/to/this/script/csem_local.sh
-# 3. Restart Bash for the settings to take effect.
-# 4. (optional) Set up passwordless login by running csem-passwordless-setup
-#
-# Usage:
-#
-# csem
-#    Runs an interactive Bash shell on the CSEM server.
-# csem-ssh
-#    Connects to the CSEM SSH server.
-# csem-x
-#    Connects to the CSEM SSH server, using X.
-# csem-put localfile [remotefile]
-#    Puts localfile on your machine in the location specified by remotefile.
-#    If remotefile is not specified, puts it in your home directory.
-# csem-get remotefile [localfile]
-#    Downloads remotefile to the location specified by localfile.
-#    If localfile is not specified, puts it in the current working directory.
-# csem-putfolder localfolder [remotefolder]
-#    Puts localfolder on your machine in the location specified by remotefolder.
-#    If remotefolder is not specified, puts it in your home directory.
-# csem-get remotefolder [localfolder]
-#    Downloads remotefolder to the location specified by localfolder.
-#    If localfolder is not specified, puts it in the current working directory.
-# csem-try task
-#    Bundles up all the *.cpp and *.h files in the current directory, and
-#    runs them through the `try` tool for the given task.
-# csem-print file [printer]
-#    Prints file to the given printer. Supports any files that lp supports,
-#    including PDFs. If printer is not specified, defaults to 'min', since it's
-#    the quickest for me to get to from Physical Sciences.
-# csem-passwordless-setup
+# Macros for Flinders University School of Computer Science, Engineering and Mathematics
+# See csem_local.markdown for documentation
 
-alias csem-ssh="ssh -l $FAN lofty.csem.flinders.edu.au"
-alias csem-x="ssh -X -l $FAN lofty.csem.flinders.edu.au"
-alias csem="csem-ssh -t bash -i"
-csem-put(){
-	scp "$1" "$FAN@lofty.csem.flinders.edu.au:$2"
-}
-csem-get(){
-	if [ "$2" ]
-	then
-		scp "$FAN@lofty.csem.flinders.edu.au:$1" "$2"
-	else
-		scp "$FAN@lofty.csem.flinders.edu.au:$1" .
-	fi
-}
-csem-putfolder(){
-	tar czf folder-put-temp.tgz "$1"
-	csem-put folder-put-temp.tgz "$2"
-	if [ "$2" ]
-	then
-		REMOTEFILE="$2/folder-put-temp.tgz"
-	else
-		REMOTEFILE="folder-put-temp.tgz"
-	fi
-	csem-ssh "tar xzf \"$REMOTEFILE\" && /usr/bin/rm \"$REMOTEFILE\"" 
-	rm folder-put-temp.tgz
-}
-csem-getfolder(){
-	csem-ssh "tar czf ~/folder-get-temp.tgz \"$1\""
-	csem-get "~/folder-get-temp.tgz"
-	tar xzf folder-get-temp.tgz $2
-	rm folder-get-temp.tgz
-	csem-ssh "/usr/bin/rm folder-get-temp.tgz"
-}
+alias csem-x="ssh -X csem"
+alias csem="ssh csem -t bash -i"
+
 csem-try(){
 	zip remote-try-archive.zip *.cpp *.h
 	csem-put remote-try-archive.zip
 	csem-ssh "try $1 remote-try-archive.zip && /usr/bin/rm remote-try-archive.zip"
 	rm remote-try-archive.zip
 }
-csem-print(){
-	csem-put "$1" print.pdf
-	if [ "$2" ]; then
-		csem-ssh "lp -d $2 print.pdf"
-	else
-		csem-ssh "lp -d min print.pdf"
-	fi
-}
-csem-passwordless-setup(){
-	if [ -f ~/.ssh/id_rsa.pub ]
-	then
-		echo "RSA key found."
-	else
-		echo "You don't appear to have a RSA key at ~/.ssh/id_rsa.pub."
-		echo "I'm about to run a program that will generate one."
-		echo
-		ssh-keygen -t rsa
-	fi
-	csem-put ~/.ssh/id_rsa.pub id_rsa_temp.pub
-	csem-ssh "mkdir -p .ssh; cat ~/id_rsa_temp.pub >> .ssh/authorized_keys; cat ~/id_rsa_temp.pub >> .ssh/authorized_keys2; chmod 700 .ssh; chmod 640 .ssh/authorized_keys2"
-}
+
+# WARNING: Doesn't play nice with UniCard
+# Uncomment and use at your own risk
+
+#csem-print(){
+#	csem-put "$1" print.pdf
+#	if [ "$2" ]; then
+#		csem-ssh "lp -d $2 print.pdf"
+#	else
+#		csem-ssh "lp -d min print.pdf"
+#	fi
+#}
