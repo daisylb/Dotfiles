e4c3f5581f8756abf544476b12f84f9f680b1e7f
Adam Brenecki <adam@brenecki.id.au>
2012-05-26 17:43:40 +0930

    Misc edits to install script

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 # Run this from the project directory
 
-ln -s $HOME/.ssh/config $PWD/ssh/config
+ln -s $PWD/ssh/config $HOME/.ssh/config
 
-echo "
+# set up bash profile
+echo "###
 FAN=bren0094
 source $PWD/bash/csem_local.sh
 source $PWD/bash/lock.sh
 source $PWD/bash/open.sh
 source $PWD/bash/pathtools.sh
 source $PWD/bash/prompt.sh
-" > $HOME/.bashrc
+source `which virtualenvwrapper.sh`
+" >> $HOME/.bash_profile
+
+# set up virtualenv
+source `which virtualenvwrapper.sh`
+echo 'set-special-ps1 "virtualenv:`echo $VIRTUAL_ENV | sed "s|$WORKON_HOME/||"`"' >> $HOME/.virtualenvs/postactivate
