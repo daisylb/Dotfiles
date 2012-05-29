b3e134603f93287b3466c6fe19e8d73123c43f0c
Adam Brenecki <adam@brenecki.id.au>
2012-05-29 16:23:18 +0930

    Added install script

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 # Run this from the project directory
 
+# SSH
 ln -s $PWD/ssh/config $HOME/.ssh/config
+
+# Git
+ln -s $PWD/git/config $HOME/.gitconfig
+ln -s $PWD/git/ignore $HOME/.gitignore_global
 
 # set up bash profile
 echo "###
