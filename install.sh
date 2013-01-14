14e836eda8eda9b8b580c920d5b77fbc19eaf4b5
Adam Brenecki <adam@brenecki.id.au>
2013-01-14 13:38:49 +1030

    Change install script to fish config

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Run this from the project directory
 
 # SSH
@@ -7,17 +9,11 @@ ln -s $PWD/ssh/config $HOME/.ssh/config
 ln -s $PWD/git/config $HOME/.gitconfig
 ln -s $PWD/git/ignore $HOME/.gitignore_global
 
-# set up bash profile
-echo "###
-FAN=bren0094
-source $PWD/bash/csem_local.sh
-source $PWD/bash/lock.sh
-source $PWD/bash/open.sh
-source $PWD/bash/pathtools.sh
-source $PWD/bash/prompt.sh
-source `which virtualenvwrapper.sh`
-" >> $HOME/.bash_profile
-
-# set up virtualenv
-source `which virtualenvwrapper.sh`
-echo 'set-special-ps1 "virtualenv:`echo $VIRTUAL_ENV | sed "s|$WORKON_HOME/||"`"' >> $HOME/.virtualenvs/postactivate
+# set up fish config
+echo "
+. $PWD/fish/prompt.fish
+. $PWD/fish/paths.fish
+. $PWD/fish/aliases.fish
+. ~/personal_projects/virtualfish/virtual.fish
+set PATH $PWD/scripts/ /usr/local/bin \$PATH
+" >> ~/.config/fish/config.fish
