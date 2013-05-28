afe1ea4e14072959942712f51032ee1e558ec9d9
Adam Brenecki <adam@brenecki.id.au>
2013-05-28 16:08:47 +0930

    allow copying SSH keys to servers using a nonstandard port

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -44,17 +44,33 @@ function ssh-copy-id
 	switch (count $argv)
 		case 1
 			set server $argv[1]
+			set port 22
 			set keyfile ~/.ssh/id_rsa.pub
 		case 2
 			set server $argv[1]
-			set keyfile $argv[2]
+			if test -z (echo $argv[2] | tr -d "[0-9]")
+				# second arg is a number
+				set port $argv[2]
+				set keyfile ~/.ssh/id_rsa.pub
+			else
+				set port 22
+				set keyfile $argv[2]
+			end
+		case 3
+			set server $argv[1]
+			set port $argv[2]
+			set keyfile $argv[3]
 		case '*'
 			echo 'Error: please supply at least one argument'
-			echo 'Usage: ssh-copy-id <server> [<pubkeyfile>]'
+			echo 'Usage: ssh-copy-id <server> [<port>] [<pubkeyfile>]'
 			return 1
 	end
+	if test ! -f $keyfile
+		echo "Keyfile $keyfile doesn't exist."
+		return 1
+	end
 	set cmd "sh -c 'mkdir -p .ssh; key=\""(cat $keyfile)"\"; echo \$key >> .ssh/authorized_keys; echo \$key >> .ssh/authorized_keys2; chmod 700 .ssh/; chmod 600 .ssh/authorized_keys .ssh/authorized_keys2'"
-	ssh $server $cmd 
+	ssh $server -p $port $cmd 
 end
 
 function mde
